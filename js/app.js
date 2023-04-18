68075494035c50514462f8d6ff9a037e3129b7ef
Luciano Angeleri <lcnang@gmail.com>
2023-04-18 10:39:10 -0300

    Se agregó la funcionalidad para eliminar las tareas de manera independiente

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 let btnAñadir =document.getElementById("btnAñadir"),
     inputTarea =document.getElementById("inputTarea"),
     listaTareas = document.getElementById("listado");
+    
 let nombreTarea;
-btnAñadir.addEventListener("click",() => agregarTarea(nombreTarea))
+let numeroTarea = 0;
 
 function agregarTarea(nombreTarea){
     nombreTarea = inputTarea.value;
@@ -10,14 +11,19 @@ function agregarTarea(nombreTarea){
         alert("Debe ingresar una tarea en el cuadro de texto")
         return;
     }
+    numeroTarea ++;
     const tarea = document.createElement("div")
     tarea.classList.add("card","text-bg-dark","rounded-5","py-2","my-3")
-    console.log(nombreTarea)
     tarea.innerHTML = `
     <div class="card-body d-flex flex-row justify-content-between">
       <h5 class="card-title text-warning fs-4">${nombreTarea}</h5>
-      <button class="btn btn-outline-danger" type="button" class="btnEliminar"><i class="bi bi-trash3 fs-1"></i></button>
+      <button class="btn btn-outline-danger" type="button" id="btnEliminar-${numeroTarea}"><i class="bi bi-trash3 fs-1"></i></button>
     </div>`;
     listaTareas.appendChild(tarea);
     inputTarea.value = "";
+    let btnEliminar = document.getElementById(`btnEliminar-${numeroTarea}`);
+    btnEliminar.addEventListener("click",()=>{
+        tarea.remove();
+    })
 }
+btnAñadir.addEventListener("click",() => agregarTarea(nombreTarea))
